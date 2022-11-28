c63d0be3ba8a3dab37c72eaa98333524d29ec9ba
Darmolyna <Darmolynconcept@gmail.com>
2022-11-28 15:46:26 +0100

    pushing updated java class

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,9 +1,13 @@
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Calendar {
 	public static void main(String[] args) throws InterruptedException {
@@ -15,14 +19,26 @@ public class Calendar {
 		driver.get("https://www.path2usa.com/travel-companions");
 		
 		//month name and date
-		driver.findElement(By.id("//input [@id='form-field-travel_comp_date']")).click();
-		driver.findElement(By.xpath("//span [@class='flatpickr-day selected']")).click();
+		Thread.sleep(5000);
 		
-		List<WebElement> dates = driver.findElements(By.className(".day"));
-		int count = driver.findElements(By.className(".day")).size();
+		//driver.findElement(By.cssSelector("#form-field-travel_comp_date")).click();
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(10000));
+		w.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#form-field-travel_comp_date")));
+		//driver.findElement(By.cssSelector("#form-field-travel_comp_date")).click();
+		
+		WebElement element = driver.findElement(By.cssSelector("#form-field-travel_comp_date"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element).click().perform();
+		
+		List<WebElement> dates = driver.findElements(By.className("day"));
+		int count = driver.findElements(By.className("day")).size();
 		
 		for(int i=0; i<count; i++) {
-			String text =driver.findElements(By.className(".day")).get(i).getText();
+			String text = driver.findElements(By.className("day")).get(i).getText();
+			if(text.equalsIgnoreCase("30")) {
+				driver.findElements(By.className("day")).get(i).click();
+				break;
+			}
 			
 		}
 	}
