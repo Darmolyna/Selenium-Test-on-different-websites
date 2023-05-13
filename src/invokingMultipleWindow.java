8fde93a6e91ceb6d373759ef180ccc33405551bd
Darmolyna <86054851+Darmolyna@users.noreply.github.com>
2023-05-13 17:09:44 +0100

    added relativeLocator and InvokingMiltipleWindow class

---
+++ src/invokingMultipleWindow.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class invokingMultipleWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Darmolyn\\Desktop\\Projects\\Drivers\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.switchTo().newWindow(WindowType.TAB);
+        //driver.switchTo().newWindow(WindowType.WINDOW);
+        Set <String> handles = driver.getWindowHandles();
+        Iterator<String> it = handles.iterator();
+        String parentWindowId = it.next();
+        String childWindowId = it.next();
+        driver.switchTo().window(childWindowId);
+        driver.get("https://rahulshettyacademy.com/");
+        String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']"))
+        .get(1).getText();
+        driver.switchTo().window(parentWindowId);
+        driver.findElement(By.cssSelector("[name = 'name']")).sendKeys(courseName);
+        //driver.quit(); 
+	}
+
+}
